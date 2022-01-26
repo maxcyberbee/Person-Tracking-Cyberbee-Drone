b6de4fbc0ef85748fa4baa1a7feec47d4e37b58c
maxcyberbee <max@cyberbee.ai>
2022-01-26 18:09:39 +0200

    reorganize code need testing

--- track_person.py
+++ track_person.py
@@ -3,7 +3,6 @@
 from pygame.locals import *
 import posenet
 import argparse
-import configargparse
 
 
 from utils import CvFpsCalc
@@ -25,19 +24,12 @@ import threading
 import traceback
 from simple_pid import PID
 import tensorflow.compat.v1 as tf
+
+from includes import get_args, getAviNameWithDate, CalculateControl
 tf.disable_v2_behavior()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=int, default=101)
-parser.add_argument('--cam_id', type=int, default=0)
-# defaults need to be changed?
-parser.add_argument('--cam_width', type=int, default=1280)
-parser.add_argument('--cam_height', type=int, default=720)
-parser.add_argument('--scale_factor', type=float, default=0.7125)
-parser.add_argument('--file', type=str, default=None,
-                    help="Optionally use a video file instead of a live camera")
-args = parser.parse_args()
+#
 
 
 prev_flight_data = None
@@ -55,8 +47,6 @@ out_stream = cv2.VideoWriter(
     framerate,
     (1280, 720),
 )
-
-
 
 filename = "temp"
 
@@ -79,33 +69,9 @@ stop_gesture = False
 new_image_ready = False
 gesture_start_control = False
 stopped_lr = False
-current_height,speed,battery,wifi_quality = 0,0,0,0
+current_height, speed, battery, wifi_quality = 0, 0, 0, 0
 lr_timeout = 0
 time_out_LR = 500
-
-def getAviNameWithDate(nameIn="output.avi"):
-    """Needs a file ending on .avi, inserts _<date> before .avi.
-
-    If file exists, it appends a additional _number after the <date>
-    ensuring filename uniqueness at this time."""
-    if not nameIn.endswith(".avi"):
-        raise ValueError("filename must end on .avi")
-
-    filename = nameIn.replace(".avi", "_{0}.avi").format(
-        datetime.datetime.now().strftime("%Y-%m-%d"))
-
-    if os.path.isfile(filename):             # if already exists
-        # modify pattern to include a number
-        fn2 = filename[0:-4]+'_{0}.avi'
-        count = 1
-        # increase number until file not exists
-        while os.path.isfile(fn2.format(count)):
-            count += 1
-        # return file with number in it
-        return fn2.format(count)
-
-    else:   # filename ok, return it
-        return filename
 
 
 def controller_thread():
@@ -122,7 +88,7 @@ def controller_thread():
     global stop_gesture
     global gesture_start_control
     global lr_timeout
-    global stopped_lr 
+    global stopped_lr
     # initialize previous drone control inputs
     control_on = True  # allows you to toggle control so that you can force landing
     pdrone_cc = -111
@@ -213,8 +179,6 @@ def controller_thread():
                 print("-----------------landing-----------------------")
                 drone.land()
                 control_on = False  # disable control
-                # shutdown = True
-                # run_controller_thread = False
                 start_hand_landing = False
                 took_off = False
                 ready_to_land = False
@@ -229,8 +193,9 @@ def controller_thread():
             if(gesture_start_control):
                 gesture_start_control = False
                 control_on = True  # disable control
-                
-            if(time_out_LR < round(time.time() * 1000) - lr_timeout  and not stopped_lr): # move right/left finished
+
+            # move right/left finished
+            if(time_out_LR < round(time.time() * 1000) - lr_timeout and not stopped_lr):
                 drone.left(0)
                 stopped_lr = True
 
@@ -246,8 +211,8 @@ def controller_thread():
 
 def handler(event, sender, data, **args):
     global prev_flight_data
-    global current_height,speed,battery,wifi_quality             
-                          
+    global current_height, speed, battery, wifi_quality
+
     drone = sender
     if event is drone.EVENT_FLIGHT_DATA:
         if prev_flight_data != str(data):
@@ -259,38 +224,29 @@ def handler(event, sender, data, **args):
             speed = int(mylist[6])
             battery = int(mylist[9])
             wifi_quality = int(mylist[12])
-            print("ALT: ",current_height,"speed",speed,"battery",battery,"wifi_quality",wifi_quality)
+            print("ALT: ", current_height, "speed", speed,
+                  "battery", battery, "wifi_quality", wifi_quality)
             prev_flight_data = str(data)
     else:
         print('event="%s" data=%s' % (event.getname(), str(data)))
 
 
-def get_args():
-    print('## Reading configuration ##')
-    parser = configargparse.ArgParser(default_config_files=['config.txt'])
+def ShowVideos(frame, overlay_image, gesture_detector, number, mode, video, gesture_buffer):
 
-    parser.add('-c', '--my-config', required=False,
-               is_config_file=True, help='config file path')
-    parser.add("--device", type=int)
-    parser.add("--width", help='cap width', type=int)
-    parser.add("--height", help='cap height', type=int)
-    parser.add("--is_keyboard",
-               help='To use Keyboard control by default', type=bool)
-    parser.add('--use_static_image_mode', action='store_true',
-               help='True if running on photos')
-    parser.add("--min_detection_confidence",
-               help='min_detection_confidence',
-               type=float)
-    parser.add("--min_tracking_confidence",
-               help='min_tracking_confidence',
-               type=float)
-    parser.add("--buffer_len",
-               help='Length of gesture buffer',
-               type=int)
+    im = numpy.array(frame.to_image())
+    im = cv2.resize(im, (1280, 720))  # resize frame
+    image = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+    cv2.imshow('posenet', overlay_image)
 
-    args = parser.parse_args()
+    debug_image, gesture_id = gesture_detector.recognize(
+        image, number, mode)
+    gesture_buffer.add_gesture(gesture_id)
+    gesture_control(gesture_buffer)
+    cv2.imshow('Tello Gesture Recognition', debug_image)
 
-    return args
+    video.write(overlay_image)
+
+    cv2.waitKey(1)
 
 
 def gesture_control(gesture_buffer):
@@ -304,35 +260,20 @@ def gesture_control(gesture_buffer):
     global lr_timeout
     global stopped_lr
     gesture_id = gesture_buffer.get_gesture()
-    
+
     print("GESTURE", gesture_id)
 
-    #     # if gesture_id == 0:  # Forward
-
-    # if gesture_id == 1:  # STOP
-    #     print("-------stop-----------")
-    #     drone_cc = drone_ud = drone_fb = 0
-    #     stop_gesture = True
-    #     control_on = False  # disable control
-    #     # if gesture_id == 5:  # Back
-
     if (gesture_id == 2 or gesture_id == 0):  # UP / Forward - Take off
-        print("start_hand_landing", start_hand_landing, "took_off", took_off)
-        print("-------got up ------------")
         if(1000 < round(time.time() * 1000)-last_up_time_mili):
-            print("-------too slow ------------")
             up_count = 0
         last_up_time_mili = round(time.time() * 1000)
         up_count += 1
         if(take_of_from_gesture_count < up_count):
-            print("-------taking of ------------")
             if(not start_hand_landing or not took_off):
                 gesture_take_off = True
             elif(took_off):
                 control_on = True
                 gesture_start_control = True
-
-    #     # elif gesture_id == 4:  # DOWN
 
     if gesture_id == 3:  # LAND
         if(not start_hand_landing):
@@ -341,159 +282,18 @@ def gesture_control(gesture_buffer):
             landing_timeout = round(time.time() * 1000)
 
     elif gesture_id == 7:  # LEFT
-        if(time_out_LR < round(time.time() * 1000 )- lr_timeout):
+        if(time_out_LR < round(time.time() * 1000) - lr_timeout):
             lr_timeout = round(time.time() * 1000)
             drone.left(20)
             stopped_lr = False
+
     elif gesture_id == 6:  # RIGHT
         if(time_out_LR < round(time.time() * 1000) - lr_timeout):
             lr_timeout = round(time.time() * 1000)
             drone.right(20)
             stopped_lr = False
 
-    # elif gesture_id == -1:
-    #         print("stop - error recognition")
-    #         drone_cc = drone_ud= drone_fb=0
 
-
-def CalcWidthHight(keypoint_coords):
-    global ready_to_land
-    global last_body_height
-    leftSholy_y = int(keypoint_coords[0, 5, 0])
-    rightSholy_y = int(keypoint_coords[0, 6, 0])
-    leftHipy_y = int(keypoint_coords[0, 11, 0])
-    rightHipy_y = int(keypoint_coords[0, 12, 0])
-
-    # print("leftSholy",leftSholy,"rightSholy",rightSholy,"leftHipy",leftHipy,"rightHipy",rightHipy)
-    # technically arbitrary
-    meanHeight = int(((rightHipy_y - rightSholy_y) +
-                      (leftHipy_y - leftSholy_y))/2)
-    leftSholy_x = int(keypoint_coords[0, 5, 1])
-    rightSholy_x = int(keypoint_coords[0, 6, 1])
-    leftHipy_x = int(keypoint_coords[0, 11, 1])
-    rightHipy_x = int(keypoint_coords[0, 12, 1])
-
-    # technically arbitrary
-    meanWidth = int(((rightHipy_x - leftHipy_x) +
-                     (rightSholy_x - leftSholy_x))/2)
-    # print("x: ",keypoint_coords[0,:,1])
-    # print("y: ",keypoint_coords[0,:,0])
-    print("meanHeight", meanHeight)
-    # print("meanWidth", meanWidth)
-
-    if(start_hand_landing):
-        if(meanHeight >= landing_height):
-            ready_to_land = True
-
-    center_body_x = int(leftSholy_x+(meanWidth/2))
-    center_body_y = int(leftSholy_y+(meanHeight/2))
-    last_body_height = meanHeight
-    errorFB = meanHeight - desiredHeight
-    return center_body_x, center_body_y, errorFB
-
-
-def CalculateControl(keypoint_scores, keypoint_coords, pid_cc, pid_ud, pid_fb, overlay_image):
-    global drone_cc
-    global drone_ud
-    global drone_fb
-    global control_on
-    global last_locked_position
-    #print("------!!! calculate control!!! ----------------")
-    centerx = int(overlay_image.shape[1]/2)
-    centery = int(overlay_image.shape[0]/2)
-
-    ctrl_out_cc = 0
-    ctrl_out_ud = 0
-
-    ctrl_out_fb = 0
-
-    errorFB = 0
-    # overlay_image = cv2.putText(overlay_image, str(nosey), (120,50), cv2.FONT_HERSHEY_SIMPLEX ,   1, (55,255,45), 2)
-    errorx = 0
-    errory = 0
-
-    if control_on and ( keypoint_scores[0, 5] > .1 and keypoint_scores[0, 6] > .1 and keypoint_scores[0, 11] > .1 and keypoint_scores[0, 12] > .1):
-        center_body_x, center_body_y, errorFB=CalcWidthHight(keypoint_coords)
-
-        last_locked_position=[center_body_x, center_body_y]
-
-        overlay_image=cv2.line(overlay_image, (centerx, centery - 10),
-                                 (center_body_x, center_body_y), (255, 255, 0), 2)
-        errorx=center_body_x - centerx
-        errory=center_body_y - centery - 10
-        if abs(errorx) > 2:
-            ctrl_out_cc=pid_cc(errorx)
-            drone_cc=ctrl_out_cc
-        else:
-            drone_cc=0
-        if abs(errory) > 4:
-            ctrl_out_ud=pid_ud(errory)
-            drone_ud=ctrl_out_ud
-        else:
-            drone_ud=0
-
-            # error can be within +/- 15 without caring
-        if abs(errorFB) > 5:
-            ctrl_out_fb=pid_fb(errorFB)
-            drone_fb=ctrl_out_fb
-        else:
-            drone_fb=0
-
-    # out_img = cv2.putText(out_img, str(keypoint_scores[ii,kpoint]), (50,50), cv2.FONT_HERSHEY_SIMPLEX ,   1, (255,255,45), 2)
-    else:
-        drone_cc=0
-        drone_ud=0
-        drone_fb=0
-        pid_fb.reset()
-        pid_cc.reset()
-        pid_ud.reset()
-        #print("-----------coudn't find body -------------")
-        # reset pid
-        # if(took_off):
-        #     if(desiredHeight*2 < last_body_height):
-        #         drone_fb = -40
-        #     elif (desiredHeight/2 > last_body_height):
-        #         drone_fb = 40
-        #     if(240 < last_locked_position[0]):
-        #         drone_cc = -30
-        #     elif(80 > last_locked_position[0]):
-        #         drone_cc = 30
-        #     if(180 < last_locked_position[1]):
-        #         drone_ud = -40
-        #     elif(60 > last_locked_position[1]):
-        #         drone_ud = 40
-        # don't let the hips lie
-        # if keypoint_scores[0,11] < .04 and keypoint_scores[0,12] < .04:
-        #     drone_fb = -20
-        #     drone_ud = -20
-
-        # print("drone_cc ", drone_cc, "drone_ud ",
-        #       drone_ud, "drone_fb ", drone_fb)
-
-
-def ShowVideos(frame, overlay_image, gesture_detector, number, mode, video):
-    # overlay_image = cv2.putText(overlay_image, str(ctrl_out_fb), (30,110), cv2.FONT_HERSHEY_SIMPLEX ,   1, (55,255,45), 2)
-    # overlay_image = cv2.putText(overlay_image, str(ctrl_out_ud), (30,30), cv2.FONT_HERSHEY_SIMPLEX ,   1, (55,255,45), 2)
-    # overlay_image = cv2.putText(overlay_image, str(errory), (30,70), cv2.FONT_HERSHEY_SIMPLEX ,   1, (55,255,45), 2)
-    im = numpy.array(frame.to_image())
-    im = cv2.resize(im, (1280, 720))  # resize frame
-    image = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-    cv2.imshow('posenet', overlay_image)
-    # cv2.imshow('Input video', image)
-
-    debug_image, gesture_id = gesture_detector.recognize(
-        image, number, mode)
-    gesture_buffer.add_gesture(gesture_id)
-    gesture_control(gesture_buffer)
-    cv2.imshow('Tello Gesture Recognition', debug_image)
-
-    # out_video_save.write(image)
-    video.write(overlay_image)
-    # cv2.imshow('Original', image)
-    # cv2.imshow('Canny', cv2.Canny(image, 100, 200))
-
-    cv2.waitKey(1)
-    
 def Stream_Video():
     global new_image_ready
     global new_frame
@@ -508,6 +308,7 @@ def Stream_Video():
             out_stream.write(im_save)
         time.sleep(0.01)
 
+
 def main():
     global drone
 
@@ -517,12 +318,16 @@ def main():
     global battery_status
     global new_frame
     global new_image_ready
+    global drone_cc
+    global drone_ud
+    global drone_fb
+    global control_on
+    global last_locked_position
+    global ready_to_land
     args_gest = get_args()
     gesture_detector = GestureRecognition(args_gest.use_static_image_mode, args_gest.min_detection_confidence,
                                           args_gest.min_tracking_confidence)
     gesture_buffer = GestureBuffer(buffer_len=args_gest.buffer_len)
-    # FPS Measurement
-    cv_fps_calc = CvFpsCalc(buffer_len=10)
 
     mode = 0
     number = -1
@@ -543,7 +348,7 @@ def main():
     # drone.subscribe(drone.EVENT_VIDEO_FRAME,handler)
     print("Start Running")
     with tf.Session() as sess:
-        model_cfg, model_outputs = posenet.load_model(args.model, sess)
+        model_cfg, model_outputs = posenet.load_model(args_gest.model, sess)
         output_stride = model_cfg['output_stride']
 
         try:
@@ -552,15 +357,12 @@ def main():
             threading.Thread(target=Stream_Video).start()
             container = av.open(drone.get_video_stream())
             frame_count = 0
-            while not shutdown:                
+            while not shutdown:
                 for frame in container.decode(video=0):
                     frame_count = frame_count + 1
                     new_frame = frame
                     new_image_ready = True
-                    # im_save = numpy.array(new_frame.to_image())
-                    # im_save = cv2.resize(im_save, (1280, 720))
-                    # im_save = cv2.cvtColor(im_save, cv2.COLOR_RGB2BGR)
-                    # out_video_save.write(im_save)
+
                     # skip first 100 frames
                     if frame_count < 100:
                         continue
@@ -568,9 +370,8 @@ def main():
                         im = numpy.array(frame.to_image())
                         im = cv2.resize(im, (320, 240))  # resize frame
                         image = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-                        # image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
                         input_image, display_image, output_scale = posenet.process_input(
-                            image, scale_factor=args.scale_factor, output_stride=output_stride)
+                            image, scale_factor=args_gest.scale_factor, output_stride=output_stride)
 
                         heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
                             model_outputs,
@@ -593,10 +394,10 @@ def main():
                             display_image, pose_scores, keypoint_scores, keypoint_coords,
                             min_pose_score=0.15, min_part_score=0.1)
 
-                        CalculateControl(
-                            keypoint_scores, keypoint_coords, pid_cc, pid_ud, pid_fb, overlay_image)
-                        ShowVideos(frame, overlay_image,
-                                   gesture_detector, number, mode, video)
+                        drone_cc, drone_ud, drone_fb, control_on, last_locked_position, ready_to_land = CalculateControl(
+                            control_on, keypoint_scores, keypoint_coords, pid_cc, pid_ud, pid_fb, overlay_image, start_hand_landing, desiredHeight)
+                        ShowVideos(frame, overlay_image, gesture_detector,
+                                   number, mode, video, gesture_buffer)
 
         except KeyboardInterrupt as e:
             print(e)
